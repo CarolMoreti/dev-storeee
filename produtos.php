77704e1420e66ef46374db837bd351ee1f74bd8b
CarolMoreti <carolline.moreti1bmangal@gmail.com>
2025-04-02 15:47:13 -0300

    ,
    ,

--- produtos.php
+++ produtos.php
@@ -29,4 +29,12 @@
         $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
         echo json_encode($result, JSON_UNESCAPED_UNICODE);
     }
+    function handleGETfiltroID($pdo){
+        $filtro = $_GET['idProd'];
+        $sql ="SELECT * from tblprodutos where idProd ='$filtro'";
+        $stmt = $pdo->prepare($sql);
+        $stmt->execute();
+        $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
+        echo json_encode($result, JSON_UNESCAPED_UNICODE);
+    }
 ?>
